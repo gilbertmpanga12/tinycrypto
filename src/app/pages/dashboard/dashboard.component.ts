053b58dd64f79a00f6a23be06f33cb4180d6c308
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2021-03-04 14:54:49 +0300

    Update;- minor changes to commit

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {MatBottomSheet} from '@angular/material/bottom-sheet';
-import { MatDialog } from '@angular/material/dialog';
 import { LinkUrls } from 'src/app/models/models';
-import { DialogviewComponent } from '../dialogview/dialogview.component';
 import { InvestsheetmenuComponent } from '../invest/investsheetmenu/investsheetmenu.component';
 
 
@@ -27,25 +25,13 @@ export class DashboardComponent implements OnInit {
     }
   ];
 
-  constructor(private _bottomSheet: MatBottomSheet, public dialog: MatDialog) { }
+  constructor(private _bottomSheet: MatBottomSheet) { }
 
   ngOnInit(): void {
   }
 
-  openDialog(): void {
-    const dialogRef = this.dialog.open(DialogviewComponent, {
-      width: '350px',
-      data: {}
-    });
-
-    dialogRef.afterClosed().subscribe(result => {
-      console.log('The dialog was closed');
-      // this.animal = result;
-    });
-  }
+  
   openBottomSheet(): void {
-    this._bottomSheet.open(InvestsheetmenuComponent).afterDismissed().subscribe(src => {
-      this.openDialog();
-    });
+    this._bottomSheet.open(InvestsheetmenuComponent).afterDismissed();
   }
 }
--- src/app/pages/invest/investsheetmenu/investsheetmenu.component.ts
+++ src/app/pages/invest/investsheetmenu/investsheetmenu.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {MatBottomSheetRef} from '@angular/material/bottom-sheet';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogviewComponent } from '../../dialogview/dialogview.component';
 import { InvestComponent } from '../invest.component';
 
 
@@ -10,14 +12,26 @@ import { InvestComponent } from '../invest.component';
 })
 export class InvestsheetmenuComponent implements OnInit {
 
-  constructor(private _bottomSheetRef: MatBottomSheetRef<InvestComponent>) { }
+  constructor(private _bottomSheetRef: MatBottomSheetRef<InvestComponent>, public dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
 
   openLink(event: MouseEvent): void {
     this._bottomSheetRef.dismiss();
+    this.openDialog();
     event.preventDefault();
   }
 
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogviewComponent, {
+      width: '350px',
+      data: {}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      // this.animal = result;
+    });
+  }
 }
