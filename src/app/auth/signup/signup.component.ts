0001297f2b7c5c305f750522f89b5bf76407c81c
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2021-03-04 12:51:36 +0300

    Update;- configured signupForm logic

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
-
+  hide:boolean = true;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  signUp(f: NgForm){
+
+  }
 }
