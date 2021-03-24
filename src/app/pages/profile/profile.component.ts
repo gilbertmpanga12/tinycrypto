1a65f5c56a9e636d816b52912dd5eb47bdc6c24d
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2021-03-24 23:15:11 +0300

    Update;- completed profile feature

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -14,35 +14,8 @@ interface Section {
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
-  folders: Section[] = [
-    {
-      name: 'Auto Investments',
-      updated: new Date('1/1/16'),
-      icon: 'savings'
-    },
-    {
-      name: 'Change Username',
-      updated: new Date('1/17/16'),
-      icon: 'account_circle'
-    },
-    {
-      name: 'Work',
-      updated: new Date('1/28/16'),
-      icon: 'work'
-    }
-  ];
-  notes: Section[] = [
-    {
-      name: 'Change Password',
-      updated: new Date('2/20/16'),
-      icon: 'password'
-    },
-    {
-      name: 'Download Private Key',
-      updated: new Date('1/18/16'),
-      icon: 'vpn_key'
-    }
-  ];
+  currencies: string[] = ['UGX', 'USD', 'GBP', 'EUR'];
+  currency: string = 'UGX';
   constructor() { }
 
   ngOnInit(): void {
