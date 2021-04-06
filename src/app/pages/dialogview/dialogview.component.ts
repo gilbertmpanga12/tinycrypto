db9e24c72f1604e9f34d476378795ef0dc5e58d1
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2021-04-06 23:23:42 +0300

    Update;- generated warning module

--- src/app/pages/dialogview/dialogview.component.ts
+++ src/app/pages/dialogview/dialogview.component.ts
@@ -1,4 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
@@ -10,6 +11,11 @@ export class DialogviewComponent implements OnInit {
   hide = true;
   defaultSymbol: string = '$';
   tradeOperation: boolean;
+  togglePurchase: boolean = false;
+  sellGroup: FormGroup;
+  buyGroup: FormGroup;
+  finalTotalBtcConverted: string = '0';
+  availableBtcTotal: string = '0';
   currencies: {name: string, symbol:string}[] = [
     {name: 'USD', symbol: '$'},
     {name: 'UGX', symbol: 'UGX'},
@@ -18,14 +24,50 @@ export class DialogviewComponent implements OnInit {
   ];
   currency: string = 'UGX';
   constructor(public dialogRef: MatDialogRef<DialogviewComponent>, 
-    @Inject(MAT_DIALOG_DATA) public data: {trade: boolean}) {
+    @Inject(MAT_DIALOG_DATA) public data: {trade: boolean}, private _fb: FormBuilder) {
       this.tradeOperation = this.data.trade;
+      this.sellGroup = this._fb.group({
+        amount: [''],
+        currency: ['']
+      });
+
+      this.buyGroup = this._fb.group({
+        amount: [''],
+        currency: ['']
+      });
      }
 
   ngOnInit(): void {
-    
+    this.sellGroup.get('amount')?.valueChanges.subscribe(amount => {
+      if(amount){
+        this.finalTotalBtcConverted = amount;
+      }else{
+        this.finalTotalBtcConverted = '0';
+      }
+    });
+
+    this.buyGroup.get('amount')?.valueChanges.subscribe(amount => {
+      if(amount){
+        this.availableBtcTotal = amount;
+      }else{
+        this.availableBtcTotal = '0';
+      }
+    });
   }
 
 
+  undoPurchase(): void{
+    this.tradeOperation = true;
+    this.togglePurchase = false;
+  }
+
+  setPurchase(): void{
+    this.tradeOperation = false;
+    this.togglePurchase = true;
+  }
+
+  buyBtc(): void{
+    
+  }
 
 }
---
+++ src/app/shared/warning/warning.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { WarningComponent } from './warning/warning.component';
+
+
+
+@NgModule({
+  declarations: [WarningComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class WarningModule { }
